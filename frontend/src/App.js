d2c4b54e8f77f6d137f5d233e4f1fc744cc6260b
Ibraheem Ogundele <ogundeleibraheem@gmail.com>
2022-05-21 03:34:15 +0100

    starting Dambass website

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Da'mbass</a>
+      </header>
+
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
