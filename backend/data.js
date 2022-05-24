d9c226c02f393682e7fcd6481ca30ee0f59d3b2a
Ibraheem Ogundele <ogundeleibraheem@gmail.com>
2022-05-24 15:07:12 +0100

    add a API server using express

---
+++ backend/data.js
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      name: 'productName',
+      slug: 'productSlug1',
+      category: 'productCat',
+      image: '/images/img4.png',
+      price: 2500,
+      brand: 'productBrand',
+      rating: 4.5,
+      numReview: 10,
+      description: 'prodcutDescrpt',
+    },
+    {
+      name: 'productName',
+      slug: 'productSlug2',
+      category: 'productCat',
+      image: 'images/img2.png',
+      price: 2500,
+      brand: 'productBrand',
+      rating: 4.5,
+      numReview: 10,
+      description: 'prodcutDescrpt',
+    },
+    {
+      name: 'productName',
+      slug: 'productSlug3',
+      category: 'productCat',
+      image: 'images/img3.png',
+      price: 2500,
+      brand: 'productBrand',
+      rating: 4.5,
+      numReview: 10,
+      description: 'prodcutDescrpt',
+    },
+    {
+      name: 'productName',
+      slug: 'productSlug4',
+      category: 'productCat',
+      image: 'images/img4.png',
+      price: 2500,
+      brand: 'productBrand',
+      rating: 4.5,
+      numReview: 10,
+      description: 'prodcutDescrpt',
+    },
+  ],
+};
+
+export default data;
